ab762c1ba302bf62cf417d263950bad0c6f9e2a9
HiromiShikata <shikata.hiromi@gmail.com>
2025-02-23 06:32:17 +0000

    prep: define cli interface

--- src/adapter/entry-points/cli/index.ts
+++ src/adapter/entry-points/cli/index.ts
@@ -3,11 +3,22 @@ import { Command } from 'commander';
 
 const program = new Command();
 program
-  .argument('<path>', 'Path of example')
-  .name('Example CLI')
-  .description('This is an example')
-  .action(async (path: string) => {
-    console.log(JSON.stringify(path));
+  .option('-p, --proxyUrl <proxyUrl>', 'Proxy URL. example: https://user:password@proxy.example.com:8080')
+  .option(
+    '--apiKeyToDeploy <apiKeyToDeploy>',
+    'API Key to deploy proxy script to cloudflare workers',
+  )
+  .option('--apiKeyToAuth <apiKeyToAuth>', 'API Key to authenticate the user')
+  .name('Deploy web proxy script to cloudflare workers')
+  .description(
+    'This command will deploy the web proxy script to cloudflare workers',
+  )
+  .action(async (options) => {
+    console.log(JSON.stringify({
+      proxyUrl: options.proxyUrl,
+      apiKeyToDeploy: options.apiKeyToDeploy,
+      apiKeyToAuth: options.apiKeyToAuth
+    }));
   });
 if (process.argv) {
   program.parse(process.argv);
